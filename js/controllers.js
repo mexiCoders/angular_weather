d2b888b29a99eb051c12aea6d4f2492f0e3cbec8
Roberto Aguilar <raguilar@dojogeek.io>
2015-04-27 15:52:16 -0500

    Handle server not responding and city not found

--- js/controllers.js
+++ js/controllers.js
@@ -4,12 +4,20 @@
         .controller('indexController', ['$http', function ($http) {
             var self = this;
             this.getWeatherByCityName = function (cityName) {
-                var apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName
+                var apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName;
+                self.error = undefined;
+                self.weather = undefined;
                 $http({
                     method: 'GET',
                     url: apiUrl
                 }).success(function (data) {
-                    self.weather = data;
+                    if (data.cod && data.cod === '404') {
+                        self.error = '"' + cityName + '"' + ' not found, enter a valid city or zip code';
+                    } else {
+                        self.weather = data;
+                    }
+                }).error(function () {
+                    self.error = 'There was an error trying to fetch "' + cityName + '"';
                 });
             };
         }]);
